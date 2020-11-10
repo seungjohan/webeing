eac7db20ff363de0d359b1b6954a6101e27f99b3
seungjohan <cmostrillion@gmail.com>
2020-11-10 14:30:40 +0900

    commit before deploy with aws

--- Webeing/wsgi.py
+++ Webeing/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for webeing project.
+WSGI config for Webeing project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webeing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Webeing.settings')
 
 application = get_wsgi_application()
