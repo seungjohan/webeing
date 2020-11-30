89374c38b94369e625b82297bfc76546a9877b68
seungjohan <cmostrillion@gmail.com>
2020-11-30 15:13:34 +0900

    [ADD] setting robots.txt

--- Webeing/settings.py
+++ Webeing/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     'jquery',
     'mathfilters',
 
+    
+
 ]
 
 MIDDLEWARE = [
--- Webeing/urls.py
+++ Webeing/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('wbtotheworld/', admin.site.urls),
@@ -12,6 +14,10 @@ urlpatterns = [
     path('cart/',include('cart.urls')),
     path('coupon/',include('coupon.urls')),
     path('order/',include('order.urls')),
+
+    #seo-robots.txt
+    path('robots.txt/', TemplateView.as_view(template_name="robots.txt", 
+        content_type='text/plain')),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
