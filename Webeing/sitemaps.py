1e292cc9676b2539737ba5d44e2474c789f2150d
seungjohan <cmostrillion@gmail.com>
2020-12-01 11:55:31 +0900

    [FIX] setting sitemap n robots.txt

--- Webeing/sitemaps.py
+++ Webeing/sitemaps.py
@@ -5,30 +5,40 @@ from django.contrib.sitemaps import Sitemap
 class StaticViewSitemap(Sitemap): 
     priority = 0.64
     changefreq = 'weekly' 
-
     def items(self): 
         return [ 
-            #shop
-            'shop:serviceintroduce',
-            'shop:envigoods',
+                # '',
+                #shop
+                # 'shop:fruitvegetable',
+                # 'shop:mealsidedish',
+                # 'shop:cafedessert',
+                # 'shop:timesale',
+                # 'shop:nongbunefirst',
+                # 'shop:nongbunesecond',
+                # 'shop:mmonlightfruits',
+                # 'shop:sweetmacaroon',
+                # 'shop:myhousesidedishes',
+                'shop:serviceintroduce',
+                'shop:envigoods',
 
-            #Account
-            'Account:tos_seller_use',
-            'Account:tos_seller_private',
-            'Account:tos_user_use',
-            'Account:tos_user_private',
-            'Account:signupcustomer',
-            'Account:signupseller',
-            'Account:signup',
-            'Account:login',
-            'Account:customerPage',
+                #Account
+                'Account:tos_seller_use',
+                'Account:tos_seller_private',
+                'Account:tos_user_use',
+                'Account:tos_user_private',
+                'Account:signupcustomer',
+                'Account:signupseller',
+                'Account:signup',
+                'Account:login',
+                'Account:customerPage',
 
-            #order
-            'order:create',
-            'order:payment',
-            'order:cart_needs'
-    ] 
+                #order
+                'order:create',
+                'order:payment',
+                'order:cart_needs'
 
+            ] 
+        
     def location(self, item): 
         return reverse(item)
 
