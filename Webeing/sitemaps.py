92af972367ce37da44417d3628bcd6d761e048e8
seungjohan <cmostrillion@gmail.com>
2020-12-02 09:22:21 +0900

    [MERGE] merge with release-1.0.1 n delete deploy files

--- Webeing/sitemaps.py
+++ Webeing/sitemaps.py
@@ -6,7 +6,8 @@ class StaticViewSitemap(Sitemap):
     priority = 0.64
     changefreq = 'weekly' 
     def items(self): 
-        return [ 
+        return [
+
                 # '',
                 #shop
                 # 'shop:fruitvegetable',
@@ -21,24 +22,23 @@ class StaticViewSitemap(Sitemap):
                 'shop:serviceintroduce',
                 'shop:envigoods',
 
-                #Account
-                'Account:tos_seller_use',
-                'Account:tos_seller_private',
-                'Account:tos_user_use',
-                'Account:tos_user_private',
-                'Account:signupcustomer',
-                'Account:signupseller',
-                'Account:signup',
-                'Account:login',
-                'Account:customerPage',
+                # #Account
+                # 'Account:tos_seller_use',
+                # 'Account:tos_seller_private',
+                # 'Account:tos_user_use',
+                # 'Account:tos_user_private',
+                # 'Account:signupcustomer',
+                # 'Account:signupseller',
+                # 'Account:signup',
+                # 'Account:login',
+                # 'Account:customerPage',
 
-                #order
-                'order:create',
-                'order:payment',
-                'order:cart_needs'
+                # #order
+                # 'order:create',
+                # 'order:payment',
+                # # 'order:cart_needs'
 
             ] 
         
     def location(self, item): 
         return reverse(item)
-
